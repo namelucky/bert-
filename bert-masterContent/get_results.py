b7f4b37545a88a6f831d8435aad305abe90f7c2d
nameluck <37497053+nameluck@users.noreply.github.com>
2019-11-18 19:30:20 +0800

    Update get_results.py
    修改了get_results.py

--- bert-masterContent/get_results.py
+++ bert-masterContent/get_results.py
@@ -4,24 +4,27 @@ import pandas as pd
 
 if __name__ == '__main__':
     path = "output/"
-    pd_all = pd.read_csv(os.path.join(path, "test_results.tsv") ,sep='\t',header=None)
-    data = pd.DataFrame(columns=['label'])
-    print(pd_all.shape)
+    test_label = pd.read_csv(os.path.join(path, "test_results.tsv") ,sep='\t',header=None)
+    test=pd.read_csv("data/test.tsv",sep='\t',header=None)#读取测试集数据
+    id=test[0]#测试集第0列是id
+    final=[]
+    #print(test.shape)
+    #print(id)
 
-    for index in pd_all.index:
-        score_zero = pd_all.loc[index].values[0]
-        score_one = pd_all.loc[index].values[1]
-        score_two = pd_all.loc[index].values[2]
+#可以先看一下test_result.tsv是n行3列的，表示n组数据，3个label各个label的概率， 根据def get_labels(self)函数里的顺序
+    #第一列是label为0的概率，第二列是label为1的概率，第三列是label为2的概率，哪个概率大，认为分为哪一类
+    for index in test_label.index:
+        score_zero = test_label.loc[index].values[0]
+        score_one = test_label.loc[index].values[1]
+        score_two = test_label.loc[index].values[2]
 
         if max(score_zero, score_one, score_two) ==score_zero:
-            # data.append(pd.DataFrame([index, "neutral"],columns=['id','polarity']),ignore_index=True)
-            data.loc[index+1] = ["0"]
+            final.append([id.loc[index],0])
         elif max(score_zero, score_one, score_two) == score_one:
-            #data.append(pd.DataFrame([index, "positive"],columns=['id','polarity']),ignore_index=True)
-            data.loc[index+1] = [ "1"]
+            final.append([id.loc[index],1])
         else:
-            #data.append(pd.DataFrame([index, "negative"],columns=['id','polarity']),ignore_index=True)
-            data.loc[index+1] = [ "2"]
-        #print(negative_score, positive_score, negative_score)
+            final.append([id.loc[index],2])
+    #print(final)
+    to_final = pd.DataFrame(final)
+    to_final.to_csv(os.path.join(path, "final_result.tsv"),sep = '\t',index=None, header=['id', 'label'])  # index为false表示不保留行索引,若想分隔符为/t，可加入sep=/t
 
-    data.to_csv(os.path.join(path, "pre_sample.tsv"),sep = '\t')
